922f63f94e47a0135d8f043f01f8db239655452f
rayhan <rayhanmujumdar0177@gmail.com>
2025-02-17 15:33:36 +0600

    enable Firebase admin import and notification sending logic; add Service-Worker-Allowed header in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    async headers() {
+        return [
+            {
+                source: './public/firebase-messaging-sw.js',
+                headers: [{ key: 'Service-Worker-Allowed', value: '/' }],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
